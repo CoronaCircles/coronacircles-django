e54f66a1cfd6f3be2624a4ad452bcadd856f1a1b
Sofien Schule <school@Scholze-MacBook-Pro.local>
2020-04-10 12:27:36 +0200

    changed CoronaCIrcles texts to lowercase

--- CoronaCircles/asgi.py
+++ CoronaCircles/asgi.py
@@ -1,5 +1,5 @@
 """
-ASGI config for CoronaCircles project.
+ASGI config for coronacircles project.
 
 It exposes the ASGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'CoronaCircles.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'coronacircles.settings')
 
 application = get_asgi_application()
